521dd436fff7fdfeb789cbf675a18e1a84f6481c
hugo battilana <hugo.battilana@entrale-marseille.fr>
2018-11-08 15:22:09 +0100

    volley

--- app/src/main/java/com/example/hugob/apphugo/MainActivity.java
+++ app/src/main/java/com/example/hugob/apphugo/MainActivity.java
@@ -1,24 +1,69 @@
 package com.example.hugob.apphugo;
 
-import android.content.Intent;
-import android.os.Handler;
+
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import com.android.volley.*;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonObjectRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT=1000;
+    static String URL_API = "http://voyage2.corellis.eu/api/v2/homev2?lat=43.14554197717751&lon=6.00.246207789145&offset=0";
+
+    public String result = "RIEN";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Access the RequestQueue through your singleton class.
+
+        this.gatherData(URL_API);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent= new Intent(MainActivity.this,HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
     }
+
+    public void traitementData(String result){
+        Log.v("JSON", result);
+    }
+
+    public void gatherData(String url){
+        RequestQueue mRequestQueue;
+        // Instantiate the run
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+        // Set up the network to use HttpURLConnection as the HTTP client.
+        Network network = new BasicNetwork(new HurlStack());
+        // Instantiate the RequestQueue with the cache and network.
+        mRequestQueue = new RequestQueue(cache, network);
+        // Start the queue
+        mRequestQueue.start();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        MainActivity.this.result = response.toString();
+                        MainActivity.this.traitementData(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                    }
+                });
+        mRequestQueue.add(jsonObjectRequest);
+    }
+
 }
+
+
